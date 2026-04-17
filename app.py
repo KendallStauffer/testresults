4aba88d8704284f3f1cf9ce596960b4f295dea48
KendallStauffer <ken@ksac.com>
2026-04-17 11:31:06 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -342,7 +342,7 @@ def build_results_xml(pin, intro="Here are your milk test results."):
 
         xml += f"""
   <Speak voice="Polly.Joanna" language="en-US">
-    <prosody rate="slow">
+    <prosody rate="x-slow">
       Sample from the {ordinal(day)}.
       <break time="500ms"/>
       Butterfat {fat:.2f} percent.
@@ -357,8 +357,8 @@ def build_results_xml(pin, intro="Here are your milk test results."):
         if mun > 0:
             xml += f"""
   <Speak voice="Polly.Joanna" language="en-US">
-    <prosody rate="slow">Munn {mun}.</prosody>
-     <break time="500ms"/>
+    <prosody rate="x-slow">Munn {mun}.</prosody>
+     <break time="900ms"/>
   </Speak>"""
 
     xml += f"""
